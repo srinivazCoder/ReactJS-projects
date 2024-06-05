1ec253e4abc9214d65d05aaedf470a2707611332
srinivaschaligeri1 <srinivaschaligeri1@gmail.com>
2024-06-05 02:29:27 +0000

    accordian single selection

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,15 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Accordian from "./components/accordion/Index";
 
 function App() {
-  const [count, setCount] = useState(30)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='App'>
+
+        <Accordian />
       </div>
-      <h1>React Application</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 10)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on theVite and React logos to learn more
-      </p>
     </>
   )
 }
--- src/components/accordion/Index.jsx
+++ src/components/accordion/Index.jsx
@@ -1,7 +1,34 @@
-export default function Accordian(){
+
+//Single selection
+// Multiple selection
+
+import { useState } from "react";
+import data from "./data"
+
+export default function Accordian() {
+    const [selected, setSeleted] = useState(null);
+
+    function handleSingleSelection(getCurrentID){
+        setSeleted(getCurrentID)
+    }
+
     return (
-        <div>
-            Accordian Component
+        <div className="wrapper">
+            <div className="accordian">
+                {data && data.length > 0 ? data.map((dataItem) => (
+                    <div className="item">
+                        <div onClick={()=>handleSingleSelection(dataItem.id)} className="title">
+                            <h3>{dataItem.question}</h3>
+                            <span>+</span>
+
+                        </div>
+                        {
+                            selected === dataItem.id && <div>{dataItem.answer}</div>
+                        }
+
+                    </div>))
+                    : <div>No data found !</div>}
+            </div>
         </div>
     );
 
