1b8027e47cb8d3d7dad21fd21a5112f15ab08893
srinivaschaligeri1 <srinivaschaligeri1@gmail.com>
2024-06-21 12:12:09 +0000

    added changes for scroll compoenent

--- src/components/scroll-indicator/Index.jsx
+++ src/components/scroll-indicator/Index.jsx
@@ -1,10 +1,11 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
+import "./styles.css"
 
 export default function ScrollIndicator({ url }) {
     const [data, setData] = useState([]);
-    const [loading, setLoading] = useState(false);
+    const [loading, setLoading] = useState(true);
     const [errorMessage, setErrorMessage] = useState("");
-    const [scrollPercentage,setScrollPercentage] = useState(0);
+    const [scrollPercentage, setScrollPercentage] = useState(0);
 
     async function fetchData(getURL) {
         try {
@@ -29,30 +30,46 @@ export default function ScrollIndicator({ url }) {
         fetchData(url);
     }, [url]);
 
-    function handleScrollPercentage(){
-        console.log(
-            document.body.scrollTop,
-            document.documentElement.scrollTop,
-            document.documentElement.scrollHeight,
-            document.documentElement.clientHeight
+    function handleScrollPercentage() {
+        // console.log(
+        //     document.body.scrollTop,
+        //     document.documentElement.scrollTop,
+        //     document.documentElement.scrollHeight,
+        //     document.documentElement.clientHeight
 
-        );
+        // );
+
+        const howMuchScrolled = document.body.scrollTop || document.documentElement.scrollTop;
+        const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+
+        setScrollPercentage((howMuchScrolled / height) * 100);
     }
 
-    useEffect(()=>{
-        window.addEventListener('scroll',handleScrollPercentage);
-        return (()=>{
-            window.removeEventListener('scroll',()=>{})
+    useEffect(() => {
+        window.addEventListener('scroll', handleScrollPercentage);
+        return (() => {
+            window.removeEventListener('scroll', () => { })
         })
     })
 
-    console.log(data, loading)
+    console.log("OKAY", scrollPercentage)
+
+    if(loading){
+        return <div>Loading... Please wait...</div>
+    }
     return (<div>
 
-        <h1>Custom Scroll Indicator</h1>
+        <div className="top-conatainer">
+            <h1>Custom Scroll Indicator</h1>
+            <div className="scroll-progress-tracking-container">
+                <div className="current-progressbar" style={{ width: `${scrollPercentage}%` }}></div>
+            </div>
+        </div>
+
+
         <div className="data-container">
             {
-                data && data.length > 0  ? data.map((e)=><p>{e.title}</p>): null
+                data && data.length > 0 ? data.map((e,i) => <p key={i}>{e.title}</p>) : null
             }
         </div>
 
