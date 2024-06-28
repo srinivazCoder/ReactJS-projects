944dbc5b779bdc1a58c051e86e6022e9abc76e19
srinivaschaligeri1 <srinivaschaligeri1@gmail.com>
2024-06-28 02:28:47 +0000

    auto search component changes

--- src/components/search-auto-complete-with-API/searchAutoComplete.jsx
+++ src/components/search-auto-complete-with-API/searchAutoComplete.jsx
@@ -1,43 +1,71 @@
 import { useEffect, useState } from "react"
 
-export default function SearchAutoCompleteAPI(){
+export default function SearchAutoCompleteAPI() {
 
-    const [loading, setLoading]=useState(false);
-    const[users,setUsers] = useState([]);
-    const [error,setError] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [users, setUsers] = useState([]);
+    const [error, setError] = useState(null);
+    const [searchParam, setSearchParam] = useState("");
+    const [showDropdown, setShowDropdown] = useState(false);
+    const [filteredUsers, setFilteredUsers] = useState("");
 
-    async function fetchListOfUsers(){
-        try{
-           
+    function handleChange(event) {
+        let query = event.target.value.toLowerCase();
+        setSearchParam(query);
+
+        if (query.length > 0) {
+            const filterData = users && users.length ?
+                users.filter((u) => u.toLowerCase().includes(query))
+
+                : [];
+
+            setFilteredUsers(filterData)
+            setShowDropdown(true)
+
+
+        }else{
+             setFilteredUsers([])
+            setShowDropdown(false)
+        }
+
+    }
+
+    async function fetchListOfUsers() {
+        try {
+
             setLoading(true);
             const response = await fetch("https://dummyjson.com/users");
             const data = await response.json();
             console.log(data)
-            if(data && data.users && data.users.length){
-                setUsers(data.users);
+            if (data && data.users && data.users.length) {
+                setUsers(data.users.map((u)=>u.firstName));
                 setLoading(false);
                 setError(null);
             }
-           
 
-        }catch(e){
+
+        } catch (e) {
             setLoading(false);
             console.log(e);
             setError(e);
         }
     }
 
-    useEffect(()=>{
+    useEffect(() => {
         fetchListOfUsers()
-    },[]);
+    }, []);
 
-   
-   
+    console.log(users,filteredUsers);
+
     return (
         <div className="search-auto-complete-container">
-            <input name="search-users" placeholder="search users here"/>
+            <input value={searchParam} onChange={handleChange} name="search-users" placeholder="search users here" />
             {loading && <h6>Loading...</h6>}
             {error && <h6>Error-{error}</h6>}
+
+            {showDropdown && filteredUsers.length>0 && <ul>
+                {filteredUsers.map((e,i)=><li key={i}>{e}</li>)}
+                </ul>}
         </div>
     )
 }
